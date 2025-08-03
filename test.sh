9d2cfbf311f763db13c6df3733d933991c81e031
shellphy <ai.shellphy@gmail.com>
2025-08-03 23:03:10 +0800

    添加测试脚本

---
+++ test.sh
@@ -0,0 +1,4 @@
+f5-tts_infer-cli --model F5TTS_v1_Base \
+--ref_audio "/Users/shellphy/code/stream-mate/tts/src/f5_tts/infer/examples/basic/basic_ref_zh.wav" \
+--ref_text "对这就是我万人敬仰的太乙真人" \
+--gen_text "我太乙真人，乃是截教通天教主座下弟子，今日特来收你为徒"
